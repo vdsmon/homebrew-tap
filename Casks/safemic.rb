ea94d18a8a449a17a14a14a414724a64e6973587
Victor De Simone <59988822+vdsmon@users.noreply.github.com>
2026-07-03 10:02:30 -0300

    safemic 0.6.0 (#4)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/safemic.rb
+++ Casks/safemic.rb
@@ -1,8 +1,8 @@
 cask "safemic" do
-  version "0.5.5"
-  sha256 "14e15887778970e5a080c13819202b5217378faf0b76a3533905529bff8cfec6"
+  version "0.6.0"
+  sha256 "67a24696bf222d33a4d8fe40b8eb9ecb7da1ec85553df912b811bc786f386c31"
 
-  url "https://github.com/vdsmon/safemic/releases/download/v0.5.5/safemic-0.5.5-aarch64-apple-darwin.dmg"
+  url "https://github.com/vdsmon/safemic/releases/download/v0.6.0/safemic-0.6.0-aarch64-apple-darwin.dmg"
   name "SafeMic"
   desc "System-wide microphone mute for macOS"
   homepage "https://github.com/vdsmon/safemic"
